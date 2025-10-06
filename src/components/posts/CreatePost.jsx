ae3d42a5c6b6d9187a2d74760b082f7b67b3ff05
ArrayaWongsaita <Wongsaita.a@gmail.com>
2025-10-06 17:13:05 +0700

    feat: implement CreatePost component and integrate it into PostsPage

--- src/components/posts/CreatePost.jsx
+++ src/components/posts/CreatePost.jsx
@@ -1,3 +1,30 @@
+import { useRevalidator } from 'react-router';
+import { createPost } from '../../apis/posts-api';
+
+// src/components/posts/CreatePost.jsx
 export default function CreatePost() {
-  return <div>CreatePost</div>;
+  const revalidatePost = useRevalidator();
+
+  const hdlCreatePost = async () => {
+    const mockData = {
+      title: 'hello',
+      content: 'my content',
+      imgUrl: 'https://picsum.photos/200',
+    };
+
+    try {
+      await createPost(mockData); // apiCreatePost  CreatePostApi
+      await revalidatePost.revalidate(); // <--
+    } catch (error) {
+      console.log(error);
+      alert('create post error');
+    }
+  };
+  return (
+    <div>
+      <button className="bg-blue-400 rounded-xl" onClick={hdlCreatePost}>
+        Create post
+      </button>
+    </div>
+  );
 }
--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,9 +1,11 @@
 // src/pages/PostPage.jsx
+import CreatePost from '../components/posts/CreatePost';
 import GetPost from '../components/posts/GetPost';
 
 export default function PostsPage() {
   return (
     <div>
+      <CreatePost />
       <GetPost />
     </div>
   );
